38c2869db433b64493da49a74d14ef1b67828782
Yang Liu <liuyang12@focusmedia.cn>
2019-02-21 11:30:05 +0800

    update

--- models.py
+++ models.py
@@ -213,7 +213,7 @@ class DMNPlus(nn.Module):
         self.qa = qa
         self.word_embedding = nn.Embedding(vocab_size, hidden_size, padding_idx=0, sparse=True).cuda()
         init.uniform_(self.word_embedding.state_dict()['weight'], a=-(3 ** 0.5), b=3 ** 0.5)
-        self.criterion = nn.CrossEntropyLoss(size_average=False)
+        self.criterion = nn.CrossEntropyLoss(reduction='sum')
 
         self.input_module = InputModule(hidden_size)
         self.question_module = QuestionModule(hidden_size)
