77dcd4ab46515a9b2215c5a3d518c3c32da0ffe9
Yang Liu <liuyang12@focusmedia.cn>
2019-02-22 14:25:40 +0800

    update

--- demo.py
+++ demo.py
@@ -1,13 +1,11 @@
+import pickle
 import random
 
+import cv2 as cv
 import torch
 import torch.nn.functional as F
-from torch.autograd import Variable
-from torch.utils.data.dataloader import DataLoader
-import cv2 as cv
-import numpy as np
-from config import batch_size, device
-from data_gen import MsCocoVqaDataset
+
+from config import pickle_file, im_size
 from data_gen import pad_collate
 from utils import ensure_folder
 
@@ -17,46 +15,60 @@ if __name__ == '__main__':
     model = checkpoint['model']
     model.eval()
 
-    dset = MsCocoVqaDataset()
-    dset.set_mode('val')
-    valid_loader = DataLoader(dset, batch_size=batch_size, shuffle=False, collate_fn=pad_collate)
+    with open(pickle_file, 'rb') as file:
+        data = pickle.load(file)
 
-    chosen_samples = range(len(dset))
+    VOCAB = data['VOCAB']
+    IVOCAB = data['IVOCAB']
+    val = data['val']
+    image_ids_val, questions_val, answers_val = val
+    prefix = 'data/val2014/COCO_val2014_0000'
+
+    chosen_samples = range(len(val))
     _ids = random.sample(chosen_samples, 10)
 
     _pred_ids = []
-
-    for i, data in enumerate(valid_loader):
-        imgs, questions, targets = data
-        imgs = Variable(imgs.float().to(device))
-        questions = Variable(questions.long().to(device))
-        targets = Variable(targets.long().to(device))
-
-        max_target_len = targets.size()[1]
-        outputs = model.forward(imgs, questions, max_target_len)
-        preds = F.softmax(outputs, dim=-1)
-        _, pred_ids = torch.max(preds, dim=1)
-        _pred_ids += list(pred_ids.cpu().numpy())
-
-    print('len(_pred_ids): ' + str(len(_pred_ids)))
-
+    questions = []
+    targets = []
     ensure_folder('images')
 
-    for i, id in enumerate(_ids):
-        img = dset[id][0]
-        img = img * 128 + 127.5
-        question = dset[id][1]
-        question = ''.join([dset.QA.IVOCAB[id] for id in question]).replace('<EOS>', '')
-        target = dset[id][2]
-        target = ''.join([dset.QA.IVOCAB[id] for id in target]).replace('<EOS>', '')
+    batch = []
 
-        pred = _pred_ids[id]
-        pred = ''.join([dset.QA.IVOCAB[id] for id in pred]).replace('<EOS>', '')
-
-        img = img.astype(np.uint8)
-        img = cv.cvtColor(img, cv.COLOR_RGB2BGR)
+    for i, index in _ids:
+        image_id = int(image_ids_val[index])
+        image_id = '{:08d}'.format(image_id)
+        filename = prefix + image_id + '.jpg'
+        img = cv.imread(filename)
+        img = cv.resize(img, (im_size, im_size))
         filename = 'images/{}_img.png'.format(i)
         cv.imwrite(filename, img)
+
+        img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+        img = img.transpose(2, 0, 1)
+        img = (img - 127.5) / 128
+
+        question = questions_val[index]
+        answer = answers_val[index]
+
+        questions.append(question)
+        targets.append(answer)
+
+    data = pad_collate(batch)
+    imgs, questions, targets = data
+    max_target_len = targets.size()[1]
+    outputs = model.forward(imgs, questions, max_target_len)
+    preds = F.softmax(outputs, dim=-1)
+    _, pred_ids = torch.max(preds, dim=1)
+    _pred_ids += list(pred_ids.cpu().numpy())
+
+    for i in range(10):
+        question = questions[i]
+        question = ''.join([IVOCAB[id] for id in question]).replace('<EOS>', '')
+        target = targets[i]
+        target = ''.join([IVOCAB[id] for id in target]).replace('<EOS>', '')
+
+        pred = _pred_ids[i]
+        pred = ''.join([IVOCAB[id] for id in pred]).replace('<EOS>', '')
 
         print('提问：' + question)
         print('标准答案：' + target)
